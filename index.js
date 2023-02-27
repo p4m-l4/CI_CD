b186711c32595cdc137815a7ce207e62a4963f0f
Pamela Namala <namala.pamela@gmail.com>
2023-02-27 18:26:04 +0300

    Add index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+// Instantiation
+const app = express();
+
+// Home Route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});

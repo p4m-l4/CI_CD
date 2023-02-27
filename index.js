9be427b05d9529a54ddf4cf493666a2ae038f30f
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-27 17:38:08 +0300

    add middleware to index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import express from 'express';
 // Instantiation
 const app = express();
 
+// middleware
+app.use(express.urlencoded({ extended: true }));
+
 // Home Route
 app.get('/', (req, res) => {
   res.send('Hello, world!');

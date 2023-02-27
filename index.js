2a53af0c11fde8044ba8978f7d820be898b1cee5
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-27 18:29:12 +0300

    start on calculator

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // Required modules
 import express from 'express';
 
+// locals
+import add from './calculator/add.js';
+import subtract from './calculator/subtract.js';
+
 // Instantiation
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
 
+// Example data
+const vals = {
+  num1: 1,
+  num2: 2,
+  opperand: '+',
+};
+
+let ans;
+
+if (vals.opperand === '+') {
+  ans = add(vals.num1, vals.num2);
+  console.log(ans);
+  // return ans;
+}
 // Home Route
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.json({ ans });
 });
 
 app.listen(3000, () => {

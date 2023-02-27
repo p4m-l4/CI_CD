e35784435fa05f7d4cf0b5ba4e8b9ea150d44800
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-27 15:48:14 +0300

    add calculator app

---
+++ index.js
@@ -0,0 +1,14 @@
+// Required modules
+import express from "express";
+
+// Instantiation
+const app = express();
+
+// Home Route
+app.get("/", (req, res) => {
+	res.send("Hello, world!");
+});
+
+app.listen(3000, () => {
+	console.log("listening on port 3000");
+});

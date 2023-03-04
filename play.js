0f5b7f007e99d6484ca9832afc3b1d6c55036df4
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-03-04 13:24:52 +0300

    added scripts, code coverage

--- play.js
+++ play.js
@@ -1,8 +1,8 @@
-import path from 'path';
-import add from './calculator/add.js';
+// // import path from 'path';
+// import add from './calculator/add.js';
 
-const res = add(1, 3);
+// const res = add(1, 3);
 
-console.log(res);
+// console.log(res);
 
-console.log(process.cwd());
+// console.log(process.cwd());

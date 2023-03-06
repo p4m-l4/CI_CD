3669ff246cc2fde6137533bc40ed7276ef3ae92b
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-03-06 09:55:47 +0300

    add logger

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import mongoose from 'mongoose';
 
 // ================================= locals ========================================
 
+import logger from './logger.js';
+import httpLogger from './httpLogger.js';
 import calcRoute from './routes/calcRoute.js';
 
 // =============================== Load Environment Variables ========================
@@ -25,11 +27,21 @@ await mongoose
   // eslint-disable-next-line no-console
   .then(() => console.log('Connected to DB! 👌'));
 
+// loggers
+function logErrors(err, req, res, next) {
+  // console.error(err.stack);
+  next(err);
+}
+function errorHandler(err, req, res) {
+  res.status(500).send('Error!');
+}
+
 // app settings
 app.set('view engine', 'pug');
 app.set('views', path.join(process.cwd(), 'views'));
 
 // middleware
+app.use(httpLogger);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(process.cwd(), 'public')));
 
@@ -39,7 +51,11 @@ app.use('/', calcRoute);
 
 // listening port
 
+app.use(logErrors);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`🚩 listening on port 🙌 ${PORT} 🙌 🚩`);
+  // console.log(`🚩 listening on port 🙌 ${PORT} 🙌 🚩`);
+  logger.info(`Calc server listening on port ${PORT}`);
 });
